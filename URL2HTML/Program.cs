c28e461772367844a813563944c5c8cb1aa3cd37
Fahad Altaf <fahadaltaf10SE49@hotmail.com>
2022-10-27 09:47:18 +0400

    comments added

--- URL2HTML/Program.cs
+++ URL2HTML/Program.cs
@@ -1,12 +1,15 @@
 ﻿using HtmlAgilityPack;
 using PuppeteerSharp;
 
+//Loads url using http request
 var content = await UsingHttpRequestAsync("http://example.com");
 Console.WriteLine(content);
 
+//Load url using HtmlagilityPack Parser
 content = await UsingHtmlParserAsync("http://example.com");
 Console.WriteLine(content);
 
+//Load url using puppeteerSharp. Best for cases where you want to render Javascript
 content = await UsingBrowserAsync("http://example.com");
 Console.WriteLine(content);
 
